7789e5c1c9f5a29b455a0397bac1ac8f28443140
starlord2015 <noreen.ward@rocketmail.com>
2015-03-23 17:42:18 +0000

    Service Created

---
+++ CarService1/CarService1/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarService1
+{
+    public class Car
+    {
+        public String make { get; set; }
+        public String model { get; set; }
+        public String year { get; set; }
+    }
+}

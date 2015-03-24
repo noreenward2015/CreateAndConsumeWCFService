be4b1f3caec8a4d74efb6597fd6e5f8296aff64b
starlord2015 <noreen.ward@rocketmail.com>
2015-03-24 10:16:27 +0000

    Basic Service

---
+++ RestService/RestService/IService1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebGet]
+        string GetDataUsingMethodName(string value);
+
+        [OperationContract]
+        [WebGet (UriTemplate= "/GetData/{value}")]
+        string GetDataUsingURI(string value);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/GetImage")]
+        Stream GetImage();
+
+    
+      
+
+
+
+    }   
+}
---
+++ RestService/RestService/Service1.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Services;
+
+namespace RestService
+{
+
+    public class Service1 : IService1
+    {
+        public string GetDataUsingMethodName(string value)
+        {
+            return value + ", I am returned by method name ";
+        }
+
+
+        public string GetDataUsingURI(string value)
+        {
+            return value + ", I am returned by URI name ";
+        }
+
+        public Stream GetImage()
+        {
+            FileStream image = File.OpenRead(@"C:\Users\Noreen Ward\Pictures\CC.jpg");
+            WebOperationContext.Current.OutgoingResponse.ContentType = "image/jpeg";
+            return image;
+        }
+
+    }
+}
